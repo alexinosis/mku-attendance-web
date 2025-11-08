10ed387ed4a487acdb244093a11d89e54879668c
sykes alex <sykesalex264@gmail.com>
2025-11-08 08:08:44 +0300

    Initial deployment attempt

---
+++ src/main/java/com/mku/attendance/config/ServerConfig.java
@@ -0,0 +1,20 @@
+package com.mku.attendance.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+    
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            String port = System.getenv("PORT");
+            if (port != null && !port.isEmpty()) {
+                factory.setPort(Integer.parseInt(port));
+            }
+        };
+    }
+}
